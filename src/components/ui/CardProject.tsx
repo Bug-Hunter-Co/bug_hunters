785db2802b622f96104cfc34ed2e280322bb3aa4
Juan Cardona <juanscr24@hotmail.com>
2025-10-30 07:50:38 -0500

    feat: add card section and project section

--- src/components/ui/CardProject.tsx
+++ src/components/ui/CardProject.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-import Arrow from "@mui/icons-material/ArrowForward";
-import { font_geist } from "./fonts";
+import { font_paragraph, font_title } from "./fonts";
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 
 interface CardProjectProps {
     title: string;
     tecnologies: string[];
     description: string;
-    href: string;
+    path: string;
     src: string;
 }
 
@@ -15,11 +15,11 @@ export const CardProject = ({
     title,
     tecnologies,
     description,
-    href,
+    path,
     src,
 }: CardProjectProps) => {
     return (
-        <div className="flex flex-col shadow-green-300 overflow-hidden rounded-xl shadow-xs hover:shadow-lg transition-shadow duration-300 border border-[#65777B]/40 dark:bg-[#1E1E1E]">
+        <div className={`${font_paragraph.className} flex flex-col overflow-hidden rounded-xl shadow-xs hover:shadow-lg transition-shadow duration-300 border border-[#65777B]/40 dark:bg-[#1E1E1E]`}>
             {/* Imagen */}
             <div className="relative h-56 w-full">
                 <Image
@@ -32,39 +32,37 @@ export const CardProject = ({
             </div>
 
             {/* Contenido */}
-            <div className="flex flex-col gap-4 p-4">
-                <h3
-                    className={`text-lg font-semibold text-[#2C3E50] dark:text-[#E0E0E0] ${font_geist.className}`}
-                >
+            <div className="flex flex-col gap-4 p-4 justify-between bg-[#E5E4E4] dark:bg-[#212121]">
+                <h3 className={`${font_title.className} text-lg font-semibold text-[#2C3E50] dark:text-[#E0E0E0]`}>
                     {title}
                 </h3>
 
-                <p className="text-sm text-[#555] dark:text-[#B0B0B0] leading-relaxed">
+                {/* Descripción con toggle */}
+                <p className="text-sm text-[#555] dark:text-[#B0B0B0] line-clamp-2">
                     {description}
                 </p>
-
                 {/* Tecnologías */}
-                <div className="flex flex-wrap gap-2">
-                    {tecnologies.map((tech, index) => (
-                        <span
-                            key={index}
-                            className="px-2 py-1 text-xs font-medium rounded-md bg-[#E8F5E9] text-[#2E7D32] dark:bg-[#519872]/20 dark:text-[#A5D6A7] border border-[#519872]/30"
-                        >
-                            {tech}
-                        </span>
-                    ))}
+                <div className="flex flex-wrap gap-2 items-center justify-between">
+                    <div className="flex gap-2">
+                        {tecnologies.map((tech, index) => (
+                            <span
+                                key={index}
+                                className="px-2 py-1 text-xs font-medium rounded-xl bg-[#37484E]/80 text-white dark:bg-[#789987]/50 dark:text-white border border-[#519872]/30"
+                            >
+                                {tech}
+                            </span>
+                        ))}
+                    </div>
+                    {/* Link */}
+                    <Link
+                        href={path}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <RemoveRedEyeIcon className="text-black dark:text-white" />
+                    </Link>
                 </div>
 
-                {/* Link */}
-                <Link
-                    href={href}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="mt-2 inline-flex items-center gap-1 text-sm font-medium text-[#519872] hover:text-[#66BB6A] transition-colors"
-                >
-                    Visitar el sitio web
-                    <Arrow fontSize="small" />
-                </Link>
             </div>
         </div>
     );
