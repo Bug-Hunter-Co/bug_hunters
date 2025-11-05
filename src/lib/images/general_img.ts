9ed2136a889b9a114c1ed46f5cde8efb8bf4e17b
Juan Cardona <juanscr24@hotmail.com>
2025-11-05 18:49:58 -0500

    feat: add img and new logos

--- src/lib/images/general_img.ts
+++ src/lib/images/general_img.ts
@@ -1,4 +1,8 @@
-// Bug Hunter img
+// Bug Hunters 
+// Logo img
+export const logo_dark = 'https://res.cloudinary.com/drxfmsqbo/image/upload/f_auto,q_auto/v1762386271/logo_light_izabx9.png'
+export const logo_light = 'https://res.cloudinary.com/drxfmsqbo/image/upload/f_auto,q_auto/v1762386271/logo_dark_kzvsrb.png'
+// General img
 export const img_hero = 'https://res.cloudinary.com/drxfmsqbo/image/upload/f_auto,q_auto/v1762136871/img_one_yr9bmb.jpg'
 export const img_about = 'https://res.cloudinary.com/drxfmsqbo/image/upload/f_auto,q_auto/v1762136871/img_two_v5ezjd.jpg'
 export const img_contact = 'https://res.cloudinary.com/drxfmsqbo/image/upload/f_auto,q_auto/v1762136914/img_four_tw7sbo.jpg'
