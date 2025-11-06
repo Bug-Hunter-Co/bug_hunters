519197051f684db118274a7e6276bf6b2c6da334
Juan Cardona <juanscr24@hotmail.com>
2025-11-06 07:01:02 -0500

    add: about img

--- src/lib/images/general_img.ts
+++ src/lib/images/general_img.ts
@@ -4,5 +4,5 @@ export const logo_dark = 'https://res.cloudinary.com/drxfmsqbo/image/upload/f_au
 export const logo_light = 'https://res.cloudinary.com/drxfmsqbo/image/upload/f_auto,q_auto/v1762386271/logo_dark_kzvsrb.png'
 // General img
 export const img_hero = 'https://res.cloudinary.com/drxfmsqbo/image/upload/f_auto,q_auto/v1762136871/img_one_yr9bmb.jpg'
-export const img_about = 'https://res.cloudinary.com/drxfmsqbo/image/upload/f_auto,q_auto/v1762136871/img_two_v5ezjd.jpg'
+export const img_about = 'https://res.cloudinary.com/drxfmsqbo/image/upload/f_auto,q_auto/v1762430295/team_k3xpkq.jpg'
 export const img_contact = 'https://res.cloudinary.com/drxfmsqbo/image/upload/f_auto,q_auto/v1762136914/img_four_tw7sbo.jpg'
